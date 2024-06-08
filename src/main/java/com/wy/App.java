5e383f5e69638470ffc2b2ea5167f276be581f77
wuyu789 <52683648+wuyu789@users.noreply.github.com>
2024-06-08 18:01:54 +0800

    push commit

--- src/main/java/com/wy/App.java
+++ src/main/java/com/wy/App.java
@@ -12,5 +12,6 @@ public class App {
         System.out.println("Hello hot-fix");
         System.out.println("master test");
         System.out.println("hot-fix test");
+        System.out.println("push test");
     }
 }
